f29e1e6b509704ebb06036ff96edc4ed0a15a048
Ford Moriarty <alessandro.borghi42@gmail.com>
2023-04-25 15:28:06 +0200

    made a few changes

--- code/H2Anorm.m
+++ code/H2Anorm.m
@@ -1,6 +1,17 @@
 function [abserror,relerror] = H2Anorm(A,B,C,Ar,Br,Cr,psicom,dpsi)
-% out = H2Abarquad(A,B,C,Ar,Br,Cr,psi,dpsi)
+% [abserror,relerror] = H2Anorm(A,B,C,Ar,Br,Cr,psicom,dpsi)
 % computes the H2Abar error norm 
+% 
+% INPUTS
+% A,B,C = full order model system matrices
+% Ar,Br,Cr = reduced order model system matrices
+% psicom = conformal map
+% dpsi = derivative of the conformal map
+% 
+% OUTPUTS
+% abserror = absolute H2(\bar A^c) error norm
+% relerror = relative H2(\bar A^c) error norm
+
 n = length(A);
 r = length(Ar);
 fom = @(z) (C*((psicom(1i.*z)*eye(n)-A)\B)) .* sqrt(dpsi(1i.*z));
@@ -8,10 +19,6 @@ rom = @(z) (Cr*((psicom(1i.*z)*eye(r)-Ar)\Br)) .* sqrt(dpsi(1i.*z));
 funerror = @(z) (fom(z)-rom(z))*conj(fom(z)-rom(z));
 funfom = @(z) (fom(z)*conj(fom(z)));
 funrom = @(z) (rom(z)*conj(rom(z)));
-abserror = sqrt((1/(2*pi))*integral(funerror,-Inf,Inf,'ArrayValued',true));%,'RelTol',1e-6,'AbsTol',1e-10)));
-relerror = abserror/sqrt((1/(2*pi))*integral(funfom,-Inf,Inf,'ArrayValued',true));%,'RelTol',1e-6,'AbsTol',1e-10)));
-% fomnorm = sqrt((1/(2*pi))*integral(funfom,-Inf,Inf,'ArrayValued',true));
-% romnorm = sqrt((1/(2*pi))*integral(funrom,-Inf,Inf,'ArrayValued',true));
-
-
+abserror = sqrt((1/(2*pi))*integral(funerror,-Inf,Inf,'ArrayValued',true));
+relerror = abserror/sqrt((1/(2*pi))*integral(funfom,-Inf,Inf,'ArrayValued',true));
 end
